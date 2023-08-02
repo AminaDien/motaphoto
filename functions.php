712eeef9073dd0d09f0e438072392d087be47ad3
Dien Amina <aminadien94@gmail.com>
2023-08-03 00:15:53 +0200

    Ajout hook contact

--- functions.php
+++ functions.php
@@ -23,4 +23,24 @@ function enqueue_custom_scripts() {
   }
   
   add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
-  
+
+ // Hook contact
+ 
+ function add_search_form2($items, $args)
+{
+    if ($args->theme_location == 'main-menu') {
+        $items .= '<button id="myBtn" class="myBtn contact header " > CONTACT </button>';
+    } else {
+    }
+
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_search_form2', 10, 2);
+
+function montheme_supports()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption')); }
+add_action('after_setup_theme', 'montheme_supports');
