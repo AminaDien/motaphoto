314ad73ab82df876950e1dbfe3398b60ef1b86ea
Dien Amina <aminadien94@gmail.com>
2023-08-03 00:16:56 +0200

    réglage parties 1 et 3

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 <main class="main">
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
-      <div class="post">
+    <div class="post">
 		<div class="post-info">
 			<h1><?php the_title(); ?></h1>
 				RÉFÉRENCE : <span id="ref-photo"> <?php echo get_field ('reference');?></span><br>
@@ -11,40 +11,45 @@
 				TYPE :  <?php echo get_field('type');?><br>
        		 	ANNÉE :<?php the_date(' Y'); ?><br>
 		</div>
-		<div class="post-content">
-          		<?php the_content(); ?>
-    	</div>
+		<?php if (has_post_thumbnail()): ?>
+        <img src="<?php the_post_thumbnail_url(array(500, 500)); ?>" alt="<?php the_title_attribute(); ?>" class="post-thumbnail" />
+        <?php endif; ?>
       </div>
 	  <div class="sous_partie">
 			<p>Cette photo vous intéresse ?
-			<a href="http://motaphoto.local/contact/"><button id="lienmodale">Contact</button></a>
+			<a><button class="myBtn" id="lienmodale">Contact</button></a>
 			</p>
-			<div class="carr">
-				<?php the_post_thumbnail(); ?>
-				<p class="carrousel">
-					<?php $prevPost = get_previous_post();
-					$prevLink = get_permalink($prevPost);?>
-					<a href="<?php echo $prevLink; ?>">
-						<img class="arrow_left"src="<?php echo get_template_directory_uri(); ?>/assets/gauche.jpg" alt="fleche_gauche">
-					</a>
-					<?php $prevNext = get_next_post();
-					$prevLinkNext = get_permalink($prevNext);?>
-					<a href="<?php echo $prevLinkNext; ?>">
-						<img class="arrow_right" src="<?php echo get_template_directory_uri(); ?>/assets/droite.jpg" alt="fleche_droite">
-					</a>
-				</p>
-			</div>
+			<div class="photo_choix">
+                    <div class="photo_avant">
+                        <?php
+                        $prev_post = get_previous_post();
+                        $next_post = get_next_post();
+
+                        if (!empty($prev_post)) {
+                            $prev_image = get_the_post_thumbnail_url($prev_post->ID);
+                            previous_post_link('<span class="left"><a href="' . get_permalink($prev_post) . '" rel="prev"><img src="' . get_template_directory_uri() . '/assets/gauche.jpg"></a>
+							<img src="' . $prev_image . '" alt="' . $prev_post->post_title . '" width="75" height="75"/> </span>', '%title', false);
+                        }
+                        ?>
+                    </div>
+                    <div class="photo_apres">
+                        <?php
+                        if (!empty($next_post)) {
+                            $next_image = get_the_post_thumbnail_url($next_post->ID);
+                            next_post_link('<span class="right"><img src="' . $next_image . '" alt="' . $next_post->post_title . '" width="75" height="75"/> <a href="' . get_permalink($next_post) . '" rel="next"><img src="' . get_template_directory_uri() . '/assets/droite.jpg"></a></span>', '%title', false);
+                        }
+                        ?>
+                    </div>
+                </div>
 	  </div>
     <?php endwhile; ?>
   <?php endif; ?>
-  	<div>
-  		<p>VOUS AIMEREZ AUSSI</p>
-				<div  id="photosapp">
-				<?php get_template_part( 'photo_block', get_post_format() ); ?>
-				</div>
-				<a href="http://motaphoto.local/accueil/"><button class="photos" id="lienacc">Toutes les photos </button></a>
+  <div class="section3">
+        <h2>VOUS AIMEREZ AUSSI</h2>
+                <div  id="photosapp">
+	<?php include_once "templates_parts/photo_block.php"; ?>   
+  <a href="<?php echo home_url()?>"><button class="btn_photos" id="lienacc">Toutes les photos </button></a>             
 	</div>
-
 </main>
 <?php
 	get_footer();
