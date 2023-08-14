e435de973f446b2ccda4b1e9df3f65f5143d7172
Dien Amina <aminadien94@gmail.com>
2023-08-14 10:01:02 +0200

    ajout lightbox

--- functions.php
+++ functions.php
@@ -20,7 +20,8 @@ add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
 // ajout de mon JS 
 function enqueue_custom_scripts() {
     wp_enqueue_script( 'custom-scripts', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0', true );
-  }
+    wp_enqueue_script('lightbox',get_stylesheet_directory_uri() . '/lightbox.js', array(),'1.0', true);
+}
   
   add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
 
@@ -44,3 +45,27 @@ function montheme_supports()
     add_theme_support('menus');
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption')); }
 add_action('after_setup_theme', 'montheme_supports');
+
+// Fonction pour charger plus de photos en AJAX
+function load_more_photos() {
+    $page = $_POST['page'];
+    $offset = ($page - 1) * 8; // Calculer l'offset
+  
+    $query = new WP_Query(array(
+      'posts_per_page' => 8,
+      'post_type' => 'photo',
+      'offset' => $offset
+    ));
+  
+    ob_start(); // Commencer la mise en mémoire tampon de sortie
+    include get_template_directory() . '/templates_parts/photo_block.php';
+    wp_reset_postdata();
+    $response = ob_get_clean(); // Récupérer la sortie mise en mémoire tampon
+  
+    echo $response; // Renvoyer les photos au format HTML
+  
+    die(); // Terminer la requête AJAX
+  }
+  add_action('wp_ajax_load_more_photos', 'load_more_photos');
+  add_action('wp_ajax_nopriv_load_more_photos', 'load_more_photos');
+  
