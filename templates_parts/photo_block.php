08e5ff480d6115f9e3035dddda273613395b4d13
Dien Amina <aminadien94@gmail.com>
2023-08-17 00:18:33 +0200

    ajout hover

--- templates_parts/photo_block.php
+++ templates_parts/photo_block.php
@@ -2,25 +2,28 @@
    
     <div class="photo_aleatoire">
 
-            <!-- //On parcourt chacun des articles résultant de la requête -->
-            <?php $count = 0; ?>
-            <?php while ($query->have_posts()): ?>
-                <?php $count++; ?>
-                <?php $query->the_post(); ?>
+        <!-- //On parcourt chacun des articles résultant de la requête -->
+        <?php $count = 0; ?>
+        <?php while ($query->have_posts()): ?>
+            <?php $count++; ?>
+            <?php $query->the_post(); ?>
 
+            <?php the_content(); ?>
+            <?php if (has_post_thumbnail()): ?>
 
-                <?php the_content(); ?>
-                <?php if (has_post_thumbnail()): ?>
+                <div class="photo_aimerezaussi">
+                    <a href="<?php echo get_the_permalink(); ?>" class="photo_link">
 
-                    <div class="photo_aimerezaussi">
-                        <?php the_post_thumbnail(); ?>
-                       
-                    </div>
+                    <?php the_post_thumbnail(); ?>
+                        <div class="overlay">
+                            <img class="eye" src="<?php echo get_template_directory_uri(); ?>/assets/eye.png" alt="eye">
+                            <img class="fullscreen" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen">
+                        </div></a>
+                </div>
                    
-                <?php endif; ?>
+        <?php endif; ?>
 
-            <?php endwhile; ?>
-        </div>
-    
-       
+        <?php endwhile; ?>
+
+    </div>
 </section>
