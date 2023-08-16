7dbcdb3922791a69674c444c61a2a19e75fdeff4
Dien Amina <aminadien94@gmail.com>
2023-08-17 00:19:24 +0200

    debut lightbox

---
+++ lightbox.php
@@ -0,0 +1,10 @@
+<div class="lightbox" id="lightbox">
+	<button class="lightbox_close" id="close-lightbox">&times;</button>
+	<button class="lightbox_prev"> &#x2190 Précédente</button>
+	<button class="lightbox_next">Suivante &#x2192</button>
+
+	<div class="lightbox__container">
+
+	</div>
+
+</div>
