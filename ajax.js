0e1249a6a54e545e9987b91e42ce985ca37dae53
Dien Amina <aminadien94@gmail.com>
2023-08-17 00:19:14 +0200

    ajout filtres et pagination

---
+++ ajax.js
@@ -0,0 +1,40 @@
+let currentPage = 1;
+jQuery('#load-more').on('click', function () {
+  currentPage++; // Do currentPage + 1, because we want to load the next page
+
+  jQuery.ajax({
+    type: 'POST',
+    url: '/wp-admin/admin-ajax.php',
+    dataType: 'html',
+    data: {
+      action: 'weichie_load_more',
+      paged: currentPage,
+    },
+    success: function (res) {
+        jQuery('.photos_accueil').append(res);
+    }
+  });
+});
+console.log('test')
+
+jQuery('#cat1, #format1, #date1').on('change', function() {
+    var categorie = jQuery('#cat1').val();
+    var format = jQuery('#format1').val();
+    var date = jQuery('#date1').val();
+  
+    var data = {
+      action: 'filter_post',
+      categorie: categorie,
+      format: format,
+      date: date // Ajout de la clé 'date' avec la valeur sélectionnée
+    };
+  
+    jQuery.ajax({
+      type: 'POST',
+      url: '/wp-admin/admin-ajax.php',
+      data: data,
+      success: function(res) {
+        jQuery('.photos_accueil').html(res);
+      }
+    });
+  });
