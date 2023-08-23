355979430c8189091251f634f4791d9c674bb5a7
Dien Amina <aminadien94@gmail.com>
2023-08-23 19:45:47 +0200

    Hover categories/titres

--- templates_parts/photo_block.php
+++ templates_parts/photo_block.php
@@ -11,21 +11,27 @@
             <?php the_content(); ?>
             <?php if (has_post_thumbnail()): ?>
 
-                <div class="photo_aimerezaussi">
+            <div class="photo_aimerezaussi">
                 <div class="photo_link">
-                    <a href="<?php echo get_the_permalink(); ?>">
-
-                    <?php the_post_thumbnail(); ?>
-                        <div class="overlay">
+                     <a href="<?php echo get_the_permalink(); ?>">
+                     <?php the_post_thumbnail(); ?>
+                <div class="overlay">
                         <div class="diveye">
                         <a href="<?php the_permalink(); ?>"><img class="eye" src="<?php echo get_template_directory_uri(); ?>/assets/eye.png" alt="eye"></a>
                         </div>
                         <div class="divfull">
-                        <button class="open-lightbox"><img class="fullscreen open-lightbox" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen"></button>
+                        <button class="open-lightbox" data-img-url="<?php echo esc_url(get_the_post_thumbnail_url()); ?>">
+                         <img class="fullscreen open-lightbox" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen">
+                        </button>
                         </div>
-                        </div>
+                        </a>
+                    <div class="photo-details">
+                <p class="photo-title"><?php the_title(); ?></p>
+                <p class="photo-category"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?></p>
+                    </div>
                 </div>
                 </div>
+            </div>
                    
         <?php endif; ?>
 
