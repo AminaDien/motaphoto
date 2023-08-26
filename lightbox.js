37a6d20d4692e29d50a70d089b2ba78f82633531
Dien Amina <aminadien94@gmail.com>
2023-08-26 09:37:48 +0200

    dernieres modif

--- lightbox.js
+++ lightbox.js
@@ -6,24 +6,45 @@ document.addEventListener("DOMContentLoaded", function() {
   const openButton = document.querySelectorAll(".open-lightbox");
 
   // ouverture lightbox
-    openButton.forEach(function(button){
-        button.addEventListener('click', function(){
-            lightbox.style.display = "block";
-        })
-    });
+  openButton.forEach(function(button){
+    button.addEventListener('click', function(){
+        const imgUrl = button.getAttribute("data-img-url");
+        const lightboxContent = document.querySelector(".lightbox_content");
+        lightboxContent.innerHTML = `<img src="${imgUrl}" alt="image">`;
+        lightbox.style.display = "block";
+        let imagecateg = button.getAttribute('data-categ');
+        let lightboxcateg = lightbox.querySelector('.lightbox__categ');
+        lightboxcateg.textContent = imagecateg;
+  });
+});
+
+// Ajouter un tableau pour stocker les URLs des images
+const imageUrls = Array.from(openButton).map(button => button.getAttribute("data-img-url"));
+let currentIndex = 0;
+console.log(imageUrls);
+
+// Fonction pour mettre à jour le contenu de la lightbox avec une image
+function updateLightboxContentWithImage(url) {
+  const lightboxContent = document.querySelector(".lightbox_content");
+  lightboxContent.innerHTML = `<img src="${url}" alt="image">`;
+  lightbox.style.display = "block";
+}
+
+// Gestionnaire pour le bouton Suivante
+nextButton.addEventListener("click", function() {
+  currentIndex = (currentIndex + 1) % imageUrls.length;
+  updateLightboxContentWithImage(imageUrls[currentIndex]);
+});
+
+// Gestionnaire pour le bouton Précédente
+prevButton.addEventListener("click", function() {
+  currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+  updateLightboxContentWithImage(imageUrls[currentIndex]);
+});
 
   // Fermer la lightbox 
   closeButton.addEventListener("click", function() {
     lightbox.style.display = "none";
   });
 
-  // Gérer la photo précédente
-  prevButton.addEventListener("click", function() {
-    // Ajoutez ici la logique pour afficher la photo précédente
-  });
-
-  // Gérer la photo suivante
-  nextButton.addEventListener("click", function() {
-    // Ajoutez ici la logique pour afficher la photo suivante
-  });
 });
--- lightbox.php
+++ lightbox.php
@@ -3,4 +3,8 @@
     <div class="lightbox_content"></div> 
     <button class="lightbox_prev"> &#x2190 Précédente</button>
     <button class="lightbox_next">Suivante &#x2192</button>
+    <div class="lightboxrefcateg">
+    <p class="lightboxref"> </p>
+    <p class="lightbox__categ"> </p>
+    </div>  
 </div>
