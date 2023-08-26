87d8345b27f20ac0ddb1e7fe25a7fb74c762fcf5
Dien Amina <aminadien94@gmail.com>
2023-08-26 09:36:54 +0200

    derniere modif

--- templates_parts/photo_block.php
+++ templates_parts/photo_block.php
@@ -20,8 +20,21 @@
                         <a href="<?php the_permalink(); ?>"><img class="eye" src="<?php echo get_template_directory_uri(); ?>/assets/eye.png" alt="eye"></a>
                         </div>
                         <div class="divfull">
-                        <button class="open-lightbox" data-img-url="<?php echo esc_url(get_the_post_thumbnail_url()); ?>">
-                         <img class="fullscreen open-lightbox" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen">
+                        <button class="open-lightbox ouvrir-lightbox" data-img-url="<?php echo esc_url(get_the_post_thumbnail_url()); ?>"
+                        data-categ="<?php
+                               $categories = get_the_terms(get_the_ID(), 'categorie');
+                               if ($categories && !is_wp_error($categories)) {
+                                   // Vérifie si la variable $categories existe et n'est pas une erreur de WordPress
+                                   $category_names = array(); // Crée un tableau vide pour stocker les noms des catégories
+                                   foreach ($categories as $category) {
+                                       // Parcourt chaque terme de taxonomie dans $categories
+                                       $category_names[] = $category->name;
+                                       // Ajoute le nom de la catégorie courante au tableau $category_names
+                                   }
+                                   echo implode(', ', $category_names);
+                                   // Concatène les noms des catégories avec une virgule comme séparateur
+                               } ?>">
+                         <img class="fullscreen ouvrir-lightbox" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen">
                         </button>
                         </div>
                         </a>
@@ -31,7 +44,8 @@
                     </div>
                 </div>
                 </div>
-            </div>
+             </div>
+    </div>
                    
         <?php endif; ?>
 
