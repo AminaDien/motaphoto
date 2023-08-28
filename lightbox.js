7267c700723f45765ffb30339443278ce7cfe800
Dien Amina <aminadien94@gmail.com>
2023-08-29 00:11:25 +0200

    reglages lightbox

--- lightbox.js
+++ lightbox.js
@@ -6,39 +6,54 @@ document.addEventListener("DOMContentLoaded", function() {
   const openButton = document.querySelectorAll(".open-lightbox");
 
   // ouverture lightbox
-  openButton.forEach(function(button){
-    button.addEventListener('click', function(){
-        const imgUrl = button.getAttribute("data-img-url");
-        const lightboxContent = document.querySelector(".lightbox_content");
-        lightboxContent.innerHTML = `<img src="${imgUrl}" alt="image">`;
-        lightbox.style.display = "block";
-        let imagecateg = button.getAttribute('data-categ');
-        let lightboxcateg = lightbox.querySelector('.lightbox__categ');
-        lightboxcateg.textContent = imagecateg;
+  openButton.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const imgUrl = button.getAttribute("data-img-url");
+      const lightboxContent = document.querySelector(".lightbox_content");
+      lightboxContent.innerHTML = `<img src="${imgUrl}" alt="image">`;
+      lightbox.style.display = "block";
+      let imageRef = button.getAttribute('data-ref');
+      let lightboxRef = lightbox.querySelector('.lightboxref');
+      lightboxRef.textContent = `${imageRef}`;
+      let imageCateg = button.getAttribute('data-categ');
+      let lightboxCateg = lightbox.querySelector('.lightbox__categ');
+      lightboxCateg.textContent = `${imageCateg}`;
+    });
   });
-});
 
 // Ajouter un tableau pour stocker les URLs des images
 const imageUrls = Array.from(openButton).map(button => button.getAttribute("data-img-url"));
 let currentIndex = 0;
 
-// Fonction pour mettre à jour le contenu de la lightbox avec une image
-function updateLightboxContentWithImage(url) {
+// Fonction pour mettre à jour le contenu de la lightbox avec une image, une référence et une catégorie
+function updateLightboxContentWithImage(url, ref, categ) {
   const lightboxContent = document.querySelector(".lightbox_content");
   lightboxContent.innerHTML = `<img src="${url}" alt="image">`;
   lightbox.style.display = "block";
+  const lightboxRef = document.querySelector(".lightboxref");
+  lightboxRef.textContent = `${ref}`;
+  const lightboxCateg = document.querySelector(".lightbox__categ");
+  lightboxCateg.textContent = `${categ}`;
 }
 
 // Gestionnaire pour le bouton Suivante
 nextButton.addEventListener("click", function() {
   currentIndex = (currentIndex + 1) % imageUrls.length;
-  updateLightboxContentWithImage(imageUrls[currentIndex]);
+  const currentImage = openButton[currentIndex];
+  const imageUrl = currentImage.getAttribute("data-img-url");
+  const imageRef = currentImage.getAttribute("data-ref");
+  const imageCateg = currentImage.getAttribute("data-categ");
+  updateLightboxContentWithImage(imageUrl, imageRef, imageCateg);
 });
 
 // Gestionnaire pour le bouton Précédente
 prevButton.addEventListener("click", function() {
   currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
-  updateLightboxContentWithImage(imageUrls[currentIndex]);
+  const currentImage = openButton[currentIndex];
+  const imageUrl = currentImage.getAttribute("data-img-url");
+  const imageRef = currentImage.getAttribute("data-ref");
+  const imageCateg = currentImage.getAttribute("data-categ");
+  updateLightboxContentWithImage(imageUrl, imageRef, imageCateg);
 });
 
   // Fermer la lightbox 
--- templates_parts/photo_block.php
+++ templates_parts/photo_block.php
@@ -13,37 +13,30 @@
 
             <div class="photo_aimerezaussi">
                 <div class="photo_link">
-                     <a href="<?php echo get_the_permalink(); ?>">
+                    <a href="<?php echo get_the_permalink(); ?>">
                      <?php the_post_thumbnail(); ?>
                 <div class="overlay">
                         <div class="diveye">
                         <a href="<?php the_permalink(); ?>"><img class="eye" src="<?php echo get_template_directory_uri(); ?>/assets/eye.png" alt="eye"></a>
                         </div>
                         <div class="divfull">
-                        <button class="open-lightbox ouvrir-lightbox" data-img-url="<?php echo esc_url(get_the_post_thumbnail_url()); ?>"
-                        data-categ="<?php
-                               $categories = get_the_terms(get_the_ID(), 'categorie');
-                               if ($categories && !is_wp_error($categories)) {
-                                   // Vérifie si la variable $categories existe et n'est pas une erreur de WordPress
-                                   $category_names = array(); // Crée un tableau vide pour stocker les noms des catégories
-                                   foreach ($categories as $category) {
-                                       // Parcourt chaque terme de taxonomie dans $categories
-                                       $category_names[] = $category->name;
-                                       // Ajoute le nom de la catégorie courante au tableau $category_names
-                                   }
-                                   echo implode(', ', $category_names);
-                                   // Concatène les noms des catégories avec une virgule comme séparateur
-                               } ?>">
-                         <img class="fullscreen ouvrir-lightbox" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen">
+                        <button class="open-lightbox ouvrir-lightbox"
+                            data-img-url="<?php echo esc_url(get_the_post_thumbnail_url()); ?>"
+                            data-categ="<?php
+                            $categories = get_the_terms(get_the_ID(), 'categorie');
+                            if ($categories && !is_wp_error($categories)) { $category_names = array();
+                            foreach ($categories as $category) { $category_names[] = $category->name;} echo implode(', ', $category_names);} ?>"
+                            data-ref="<?php echo get_post_meta(get_the_ID(), 'reference', true); ?>">
+                            <img class="fullscreen ouvrir-lightbox" src="<?php echo get_template_directory_uri(); ?>/assets/fullscreen.png" alt="fullscreen">
                         </button>
                         </div>
-                        </a>
+                    </a>
                     <div class="photo-details">
-                <p class="photo-title"><?php the_title(); ?></p>
-                <p class="photo-category"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?></p>
+                        <p class="photo-title"><?php the_title(); ?></p>
+                        <p class="photo-category"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?></p>
                     </div>
                 </div>
-                </div>
+            </div>
     </div>
                    
         <?php endif; ?>
